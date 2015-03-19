fe7d7489fc09b7d0bceb9a807b5c0308c04cce06
zzy <zzy@cs.duke.edu>
2015-03-18 21:45:55 -0400

    detect script large change

--- wpt/NonEmptyFileFinder.java
+++ wpt/NonEmptyFileFinder.java
@@ -10,6 +10,7 @@ import util.Util;
 public class NonEmptyFileFinder
 {
 	static final String ROOT = "/Users/zzy/Documents/script/new/";
+	static final int MIN_LINE = 5;
 	
 	public static void main(String args[]) throws IOException
 	{
@@ -20,9 +21,10 @@ public class NonEmptyFileFinder
 			System.exit(1);
 		}
 		
-		int total = 0, active = 0;
-		int totalPair = 0, activePair = 0;
-		boolean find = false;
+		// large: files no less then MIN_LINE lines
+		int total = 0, active = 0, active_large = 0;
+		int totalPair = 0, activePair = 0, activePair_large = 0;
+		boolean find = false, find_large = false;
 		for (File d : dir.listFiles())
 		{
 			if (d.getName().equals(".DS_Store"))
@@ -47,11 +49,11 @@ public class NonEmptyFileFinder
 			}
 			if(resultDir == null)
 			{
-				System.err.println("No result folder in "+d.getAbsolutePath());
+				// System.err.println("No result folder in "+d.getAbsolutePath());
 				continue;
 			}
 			total++;
-			find = false;
+			find = false; find_large = false;
 			
 			for(File r : resultDir.listFiles())
 			{
@@ -61,12 +63,22 @@ public class NonEmptyFileFinder
 				String content = Util.readFile(r.getAbsolutePath());
 				if(content.length() > 0)
 				{
+					int line = content.split("\n").length - 1;
 					if(!find)
 					{
 						active++;
 						find = true;
 					}
+					if(!find_large && line > MIN_LINE)
+					{
+						active_large++;
+						find_large = true;
+					}
 					activePair++;
+					if(line > MIN_LINE)
+					{
+						activePair_large++;
+					}
 					System.out.println("Not empty file: "+r.getAbsolutePath());
 				}
 			}
@@ -74,7 +86,9 @@ public class NonEmptyFileFinder
 		
 		System.out.println("Total: "+total);
 		System.out.println("Active: "+active);
+		System.out.println("Active large: "+active_large);
 		System.out.println("Total pair: "+totalPair);
 		System.out.println("Active pair: "+activePair);
+		System.out.println("Active pair large: "+activePair_large);
 	}
 }
