e8e716201aba6ff295ccc74d125e6de97afbcf46
zzy <zzy@cs.duke.edu>
2015-04-24 16:19:04 -0400

    add path

--- get_in_turn.sh
+++ get_in_turn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 url="http://skylynx.cs.duke.edu/web2/alvin/"
+path="/home/zzy/site/web2/alvin/index_files/"
 
 # rm -r ~/try/del/bg/
 # cd ~/try/del/
@@ -16,7 +17,7 @@ ret_code=0
 
 while [ $ret_code -eq 0 ]; do
 	# cd ~/try/del/
-	ssh zzy@skylynx.cs.duke.edu -t java DelInTurn
+	ssh zzy@skylynx.cs.duke.edu -t java DelInTurn $path
 	ret_code=$?
 	# mkdir delta
 	# cd delta
